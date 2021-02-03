881889e8bc95be5664b0ba6782d307eb4660065f
shiho-aoki <s.aoki.ase@gmail.com>
2021-02-03 12:05:12 +0900

    add coims

--- BiginerSection/coins.cpp
+++ BiginerSection/coins.cpp
@@ -1,10 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int A,B,C,X;
-    cin >> A;
-    cin >> B;
-    cin >> C;
-    cin >> X;
+    int A,B,C,X, data_1;
+    int data[3];
+    cin >> A >> B >>C >>X;
+    int res =0;
+    for (int a=0; a<=A; ++a){
+        for (int b=0; b<=B; ++b){
+            for(int c=0; c<=C; ++c){
+                int total = 500*a +100*b +50*c;
+                if (total ==X) ++res;
+            }
+        }
+    }
+    cout << res << endl;
     return 0;
 }
