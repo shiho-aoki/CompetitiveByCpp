090b745782da66296da3ab2c8abe8092c93a5478
shiho-aoki <s.aoki.ase@gmail.com>
2021-09-27 07:03:44 +0000

    AC: 103

---
+++ unrated/apg4b/103.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    arithmetic operator(算術演算子): +, -, /, *, %
+*/
+int main(){
+    //WA: cout << (1/2)*100*(100+1) << endl;
+    cout << 100*(100+1)/2 << endl;
+}
