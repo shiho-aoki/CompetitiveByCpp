2e33138a89a330722d0a6f39c9a1f8acfe3005f3
shiho-aoki <s.aoki.ase@gmail.com>
2021-09-27 06:38:06 +0000

    AC: 101

---
+++ unrated/apg4b/101.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h> /* import all cpp feature
+ (! this import is ok only competition) */
+using namespace std; // std:: cout -> cout, this is also ok olny competition
+
+int main(){
+    cout << "こんにちは" << endl;
+    cout << "AtCoder" << endl;
+}
