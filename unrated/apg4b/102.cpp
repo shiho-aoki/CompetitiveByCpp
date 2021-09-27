69d33263fcf9f8e96a29dd7f362ac459f2c7686b
shiho-aoki <s.aoki.ase@gmail.com>
2021-09-27 06:56:34 +0000

    AC:102

---
+++ unrated/apg4b/102.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ a compile error (=grammer error): this error is occurred by grammar, and the program does not run.
+ an error when runing: this error is occurred by contents, and the program force quit.
+ a theory error: this error is occurred by contents, and the program can run. 
+*/
+
+int main() {
+  cout << "いつも" << 2525 << endl;
+  cout << "AtCoderくん" << endl;
+}
