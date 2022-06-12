c6c393e553750f36cebfb2aceaab0ae493572eb8
s.aoki.ase <s.aoki.ase@gmail.com>
2022-06-13 05:27:32 +0900

    add 008

---
+++ math-and-algo/id_008.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int N, S;
+    int counter=0;
+    cin >> N >> S;
+
+    int red_i, blue_i;
+    for(red_i=1; red_i<=N; red_i++){
+        for (blue_i=1; blue_i<=N; blue_i++){
+            if(red_i + blue_i <= S){
+                counter ++;
+            }else{
+                break;
+            }
+        }
+    }
+
+    cout << counter << endl;
+
+    return 0;
+}
