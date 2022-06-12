a29aa25656eb027b415805fd622211e091318790
s.aoki.ase <s.aoki.ase@gmail.com>
2022-06-13 05:35:49 +0900

    wip 009

---
+++ math-and-algo/id_009.cpp
@@ -0,0 +1,17 @@
+// errored (50% OK) ver 
+// FULL SEARCH ALGO
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N, S;
+    cin >> N >> S;
+    long long A[N];
+    int i;
+    for (i=0; i<N; i++){
+        cin >> A[i];
+    }
+    
+    return 0;
+}
