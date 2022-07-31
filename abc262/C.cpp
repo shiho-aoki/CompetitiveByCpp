f73b3689b7ced677e485eda9529c996be2d51b6b
s.aoki.ase <s.aoki.ase@gmail.com>
2022-08-01 04:29:15 +0900

    add; abc262-C ans

--- abc262/C.cpp
+++ abc262/C.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
@@ -51,3 +52,33 @@ int main(){
     cout << ans_counter << endl;
     return 0;
 }// namespace std;
+*/
+
+# include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    int same = 0;
+
+    for (int i=0; i<n; ++i){
+        cin >> a[i];
+        a[i] -= 1;
+        if(a[i] == i){
+            same += 1;
+        }
+    }
+
+    long long ans = (long long)same * (same -1) /2;
+    for(int i=0; i<n; ++i){
+        if(a[i]>i and a[a[i]] == i){
+            ans += 1;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
